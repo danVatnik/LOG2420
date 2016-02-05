7d2d7a97b7349f535ca254d9db776ee3399fb920
Dominik Courcelles <dominik.courcelles@gmail.com>
2016-02-05 16:42:07 -0500

    Premiere version du worker implemente

--- TD2/main.js
+++ TD2/main.js
@@ -1,40 +1,48 @@
-$(function() {
+$(function()
+{
+    var compteurDeJetons;
+    var startWorking = function()
+    {
+        //On met à jour les événements pour cliquer sur les boutons.
+        $("#commencer").off("click");
+        $("#arreter").click(stopWorking);
+        
+        //Changer le visuel des boutons
+        
+        //On part la tâche
+        compteurDeJetons = new Worker("worker.js");
+        compteurDeJetons.postMessage($("#inputText").val());
+        compteurDeJetons.onmessage = function(e)
+        {
+            //On met à jour le nombre de jetons et la progressBar
+            $("#result").val(e.data[1]);
+            $("#myProgress").css("width", e.data[0] + "%");
+            //On vérifie si le travail est terminé
+            if(e.data[0] == 100)
+            {
+                //Le travail est terminé, on écrit terminé et on change les événements des boutons.
+                compteurDeJetons.terminate();
+                $("#commencer").click(startWorking);
+                $("#arreter").off("click");
+                
+                //Changer le visuel des boutons
+                
+            }
+        }
+    }
 
-// Code à implémenter
+    var stopWorking = function()
+    {
+        //On arrête le travail et on change les événements des boutons.
+        compteurDeJetons.terminate();
+        $("#result").val("Annulé");
+        $("#commencer").click(startWorking);
+        $("#arreter").off("click");
+        
+        //Changer le visuel des boutons
+        
+    }
 
+    //On active notre bouton pour lancer le travail.
+    $("#commencer").click(startWorking);
 });
-
-
-
-
-
-
-
-var i=0;
-
-function timedCount() {
-    i=i+1;
-    document.getElementById("result").innerHTML = i;
-    setTimeout("timedCount()", 500);
-     document.getElementById("myProgress").style.width =  i + "%";
-}
-
-function startWorking() {
-    var input = document.getElementById("numberInput").value;
-    console.log(document.getElementById("myProgress").aria-valuemax);
-    document.getElementById("myProgress").max = input;
-    timedCount();
-    
-    
-   
-    
-}
-
-function stopWorking() { 
-    i=0;   
-    document.getElementById("result").innerHTML = 0;
-}
-
-
-
-
--- TD2/worker.js
+++ TD2/worker.js
@@ -2,8 +2,15 @@
 self.importScripts("CompteurJetons.js");
 var compteur = new CompteurJetons();
 
-self.addEventListener('message', function(e) {
-
-	// Code à implémenter
-  
+self.addEventListener('message', function(e)
+{
+    // Code à implémenter
+    var generateurFonction = compteur.compterJetons(e.data);
+    var valeurProgres = undefined;
+    do
+    {
+        valeurProgres = generateurFonction.next().value;
+        postMessage([valeurProgres, compteur.getJetons()]);
+    }
+    while(valeurProgres != undefined)
 }, false);
