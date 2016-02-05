399cd7ef4dbd5c48a6e33faeabf9529b4c25c186
Dan Vatnik <davat@l4714-12.info.polymtl.ca>
2016-02-05 13:44:23 -0500

    ajout de progress bar

--- TD2/main.js
+++ TD2/main.js
@@ -3,3 +3,38 @@ $(function() {
 // Code à implémenter
 
 });
+
+
+
+
+
+
+
+var i=0;
+
+function timedCount() {
+    i=i+1;
+    document.getElementById("result").innerHTML = i;
+    setTimeout("timedCount()", 500);
+     document.getElementById("myProgress").style.width =  i + "%";
+}
+
+function startWorking() {
+    var input = document.getElementById("numberInput").value;
+    console.log(document.getElementById("myProgress").aria-valuemax);
+    document.getElementById("myProgress").max = input;
+    timedCount();
+    
+    
+   
+    
+}
+
+function stopWorking() { 
+    i=0;   
+    document.getElementById("result").innerHTML = 0;
+}
+
+
+
+
